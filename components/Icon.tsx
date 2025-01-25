84bb58e5cf468acde53eba1f3c201f8e80252006
ArKharMinn <arkharminn.1@gmail.com>
2025-01-25 10:50:50 +0630

    excelsheet

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -8,9 +8,9 @@ const Icon = () => {
     <View style={tw`items-center`}>
       <View style={tw`flex-row items-center gap-1`}>
         <AntDesign name="earth" size={20} style={tw`text-blue-500`} />
-        <Text style={tw`text-[22px] font-bold text-blue-500`}>IOM</Text>
+        <Text style={tw`text-[22px] font-bold text-blue-500`}>ExcelSheet</Text>
       </View>
-      <Text style={tw`text-blue-500 text-[10px]`}>UN MIGRATION</Text>
+      <Text style={tw`text-blue-500 text-[10px]`}>data</Text>
     </View>
   );
 };
